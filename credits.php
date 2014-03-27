54f4353335d5abc224fa5c2cd6eba6e6bf641711
marlanwood <marlan_wood@yahoo.com>
2014-03-27 15:57:28 -0600

    added credits page

---
+++ credits.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<div id="titles"><div id="titlecontent">
+
+	<p><H1>EPISODE IV - GROUP 1 PROJECT</H1></p>
+	<p><H1>A NEW HOPE FOR SUMMER BREAK</H1></p>
+	<p><H2>Hey this is pretty cool. It was borrowed from the site: http://www.sitepoint.com/css3-starwars-scrolling-text/</H2></p>	
+	<p><H2>We would like to thank you for watching our game. We hope you enjoyed it. We had fun building it and being that it <u>is</u> a game we also had fun playing it.</H2></p>	
+	<p><H2>Contributors: Brandon Elfring, David Tchudy, Thomas Wight, Lance Winch and Marlan Wood</H2></p>	
+    <p><H2>We want to thank Guedo for using all of our project funds to record all of those "legit" sound effects. You just can't fake that stuff.</H2></p>	
+    <p><H2>Song Scalpel performed by Alice in Chains</H2></p>	
+
+
+</div></div>
+
